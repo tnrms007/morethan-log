68e91d4430bec1b344a7134d62f8ef82d8843d77
tnrms007 <80308035+tnrms007@users.noreply.github.com>
2024-07-24 21:00:59 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -25,8 +25,8 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://tnrms007.vercle.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
        keywords: ["Blog", "Web", "Notion"],    
